5d6dbff5b4aa047d613430d636bbaf3af9c3556f
João Vitor <54036572+jvzaniolo@users.noreply.github.com>
2021-07-28 18:19:06 -0300

    Change how it checks if brew is installed

--- scripts/homebrew.zsh
+++ scripts/homebrew.zsh
@@ -2,7 +2,7 @@
 
 echo "\n<<< Starting Brew Setup >>>\n"
 
-if command -v brew &> /dev/null; then
+if command -v brew 1>/dev/null 2>/dev/null; then
   echo "brew exists, skipping install"
 else
   echo "brew doesn't exist, continuing with install"
