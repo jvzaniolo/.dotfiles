1e0f44f021f85bfa68cca333c2fdf659d1fcdf5f
João Vitor Zaniolo <54036572+jvzaniolo@users.noreply.github.com>
2022-01-27 16:10:53 -0300

    add(theme): p10k configuration

--- install.zsh
+++ install.zsh
@@ -3,10 +3,9 @@
 ln -s ~/.dotfiles/home/.sheldon ~
 ln -s ~/.dotfiles/home/.gitconfig ~
 ln -s ~/.dotfiles/home/.zshrc ~
+ln -s ~/.dotfiles/home/.p10k ~
 
 touch ~/.hushlogin
-
-mkdir ~/Projects
 
 source "scripts/homebrew.zsh"
 source "scripts/macos.zsh"
