fd28ddc539daf2581e830a255386eec0845e5f72
João Vitor <jv.zaniolo@gmail.com>
2022-04-08 22:00:39 -0300

    add gitifnore

--- install.ps1
+++ install.ps1
@@ -1,41 +1,40 @@
 
-Echo Install Blitz.Blitz
-REM Blitz
+Echo Install Blitz
 winget install Blitz.Blitz
-if %ERRORLEVEL% EQU 0 Echo Blitz installed successfully.
+Echo Blitz installed successfully.
+
 Echo Install 1Password
-REM 1Password
 winget install AgileBits.1Password
-if %ERRORLEVEL% EQU 0 Echo 1Password installed successfully.
+Echo 1Password installed successfully.
+
 Echo Install CPU-Z
-REM CPU-Z
 winget install CPUID.CPU-Z
-if %ERRORLEVEL% EQU 0 Echo CPU-Z installed successfully.
+Echo CPU-Z installed successfully.
+
 Echo Install Discord
-REM Discord
 winget install Discord.Discord
-if %ERRORLEVEL% EQU 0 Echo Discord installed successfully.
+Echo Discord installed successfully.
+
 Echo Install Figma
-REM Figma
 winget install Figma.Figma
-if %ERRORLEVEL% EQU 0 Echo Figma installed successfully.
+Echo Figma installed successfully.
+
 Echo Install OBSStudio
-REM OBSStudio
 winget install OBSProject.OBSStudio
-if %ERRORLEVEL% EQU 0 Echo OBSStudio installed successfully.
+Echo OBSStudio installed successfully.
+
 Echo Install Steam
-REM Steam
 winget install Valve.Steam
-if %ERRORLEVEL% EQU 0 Echo Steam installed successfully.
+Echo Steam installed successfully.
+
 Echo Install Notion
-REM Notion
 winget install Notion.Notion
-if %ERRORLEVEL% EQU 0 Echo Notion installed successfully.
+Echo Notion installed successfully.
+
 Echo Install GHUB
-REM GHUB
 winget install Logitech.GHUB
-if %ERRORLEVEL% EQU 0 Echo GHUB installed successfully.
+Echo GHUB installed successfully.
+
 Echo Install Chrome
-REM Chrome
 winget install Google.Chrome
-if %ERRORLEVEL% EQU 0 Echo Chrome installed successfully.
+Echo Chrome installed successfully.
